b2327a807ddf508f0dc8bef02413d3e58c59daa6
Kulakov <kulakov22@yandex.ru>
2016-02-15 20:17:26 +0600

    Добавил список(массив) символов, и список точек

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,12 +11,37 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(1, 3, '*');
+            Point p1 = new Point(1,3,'*');
             p1.Draw();
             
 
             Point p2 = new Point(4,5,'#');
             p2.Draw();
+
+            Point p3 = new Point(6, 7, '!');
+            p3.Draw();
+
+
+            Point p4 = new Point(8, 9, '&');
+            p4.Draw();
+
+           
+            List<char> ListChar = new List<char>();
+            ListChar.Add('!');
+            ListChar.Add('@');
+            ListChar.Add('#');
+
+           foreach(char i in ListChar)
+            {
+                Console.WriteLine(i);
+            }
+           
+
+            List<Point> pList = new List<Point>();
+            pList.Add( p1 );
+            pList.Add( p2 );
+            pList.Add( p3 );
+            pList.Add( p4 );
 
             Console.ReadLine();
         }
